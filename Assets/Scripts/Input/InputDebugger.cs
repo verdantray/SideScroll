3835c5792345ca6f249450f35bdd7dce1e83f91f
Chococornets <decibe1magic@naver.com>
2023-06-04 05:43:02 +0900

    IInputHandleable 인터페이스 작성

--- Assets/Scripts/Input/InputDebugger.cs
+++ Assets/Scripts/Input/InputDebugger.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -11,51 +12,55 @@ namespace SideScroll.Input
     // Scripted by Chococornets
     // 2023. 05. 15
 
-    public class InputDebugger : MonoBehaviour
+    public interface IInputHandleable
+    {
+        public void OnMove(InputValue inputValue);
+        public void OnJump(InputValue inputValue);
+        public void OnSit(InputValue inputValue);
+        public void OnAttack(InputValue inputValue);
+        public void OnSkill(InputValue inputValue);
+        public void OnDash(InputValue inputValue);
+    }
+
+    public class InputDebugger : MonoBehaviour, IInputHandleable
     {
         private StringBuilder Logger => logger ??= new StringBuilder();
 
         private StringBuilder logger = null;
 
-        private void Start()
+        #region User Input Events
+
+        public void OnMove(InputValue inputValue)
         {
-            // 어째선지 에디터 플레이 중엔 출력 안되다가 종료 시 한꺼번에 출력되는 현상이 있음
-            // InputSystem.onDeviceChange += LogCurrentDevice;
+            float axis = inputValue.Get<float>();
+            int value = Mathf.RoundToInt(axis);
+            
+            LogMessage("Move ", value.ToString(CultureInfo.InvariantCulture));
         }
 
-        #region InGame Events
-
-        // public void OnAttack(InputAction.CallbackContext context)
-        // {
-        //     if (context.phase != InputActionPhase.Started) return;
-        //     LogMessage("Attack");
-        // }
-        //
-        // public void OnSkill(InputAction.CallbackContext context)
-        // {
-        //     if (context.phase != InputActionPhase.Started) return;
-        //     LogMessage("Skill");
-        // }
-        //
-        // public void OnEvade(InputAction.CallbackContext ctx)
-        // {
-        //     if (ctx.phase != InputActionPhase.Started) return;
-        //     LogMessage("Evade");
-        // }
-        //
-        // public void OnVertical(InputAction.CallbackContext ctx)
-        // {
-        //     if (ctx.phase != InputActionPhase.Started) return;
-        //
-        //     string verticalBehavior = ctx.ReadValue<float>() > 0.0f ? "Jump" : "Sit";
-        //
-        //     LogMessage("Move Vertical ", verticalBehavior);
-        // }
-
-        public void OnHorizontal(InputValue inputValue)
+        public void OnJump(InputValue inputValue)
         {
-            short axis = Convert.ToInt16(inputValue.Get<float>());
-            LogMessage(axis.ToString(CultureInfo.InvariantCulture));
+            LogMessage("Jump");
+        }
+
+        public void OnSit(InputValue inputValue)
+        {
+            LogMessage("Sit");
+        }
+
+        public void OnAttack(InputValue inputValue)
+        {
+            LogMessage("Attack");
+        }
+
+        public void OnSkill(InputValue inputValue)
+        {
+            LogMessage("Skill");
+        }
+
+        public void OnDash(InputValue inputValue)
+        {
+            LogMessage("Dash");
         }
 
         #endregion
---
+++ Assets/Scripts/RectSizeFitter.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SideScroll
+{
+    // 스크린 사이즈에 맞춰 UI가 화면 안에서만 표시되도록 Rect를 맞춥니다.
+    
+    [RequireComponent(typeof(RectTransform))]
+    public class RectSizeFitter : MonoBehaviour
+    {
+        [SerializeField] private RectTransform rectToFit = null;
+
+        private void Reset()
+        {
+            rectToFit = gameObject.GetComponent<RectTransform>();
+        }
+
+        private void Awake()
+        {
+            ChangeRectSize(Screen.safeArea);
+        }
+
+        private void OnRectTransformDimensionsChange()
+        {
+            ChangeRectSize(Screen.safeArea);
+        }
+
+        private void ChangeRectSize(Rect toChange)
+        {
+            Vector2 minAnchor = toChange.position;
+            Vector2 maxAnchor = minAnchor + toChange.size;
+
+            Vector2 curScreenSize = new Vector2(Screen.width, Screen.height);
+            
+            minAnchor /= curScreenSize;
+            maxAnchor /= curScreenSize;
+
+            rectToFit.anchorMin = minAnchor;
+            rectToFit.anchorMax = maxAnchor;
+        }
+    }
+}
